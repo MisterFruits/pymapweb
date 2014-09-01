b3fa0b98b20754cfd5b829e65ca779d1c6815b33
Victor <vic.toad.tor@free.fr>
2014-09-02 00:05:39 +0200

    NEW basic test web app

--- src/web.py
+++ src/web.py
@@ -1,1 +1,25 @@
-import bottle
+from bottle import Bottle, run, view, static_file
+
+app = Bottle()
+
+@app.route('/bootstrap/<filepath:path>')
+def bootstrap_static(filepath):
+    return static_file(filepath, root='../resources/bootstrap/')
+
+@app.route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='../resources/html_static/')
+
+@app.route('/')
+@app.route('/test')
+@app.route('/test/:name')
+@view('test')
+def test(name = ''):
+    return dict()
+
+@app.route('/test2')
+@view('test2')
+def test2(name = ''):
+    return dict()
+
+run(app, host='localhost', port=8080, debug=True, reloader=True)
