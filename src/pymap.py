18a5b1f0a18db0cf47ba9958756b3744402ef367
Victor <vic.toad.tor@free.fr>
2014-09-03 20:40:06 +0200

    NEW random generators for Mail and Account classes

--- src/pymap.py
+++ src/pymap.py
@@ -2,22 +2,36 @@
 Model for IMAP in python
 """
 import email
+import random
+import string
 
 class Account:
     """docstring for Account"""
-    def __init__(self, name):
+    def __init__(self, name, mails):
         self.name = name
-        self.mails = []
+        self.mails = mails
 
 class Mail(email.message.Message):
     """docstring for Mail"""
-    def __init__(self, subject, sender, body, attachement = None):
+    def __init__(self, subject, senders, body, attachements = None):
         super(Mail, self).__init__()
         self.subject = subject
-        self.sender = sender
+        self.senders = senders
         self.body = body
-        self.attachement = attachement
+        self.attachements = attachements
 
+def get_random_mail():
+    subject = gen_text(5, 'Subject %s')
+    senders = [gen_text(5, 'Sender%d %s' % el) for el in random.randrange(3)]
+    body = gen_text(random.randint(500,600))
+    return Mail(subject, senders, body)
+
+def get_random_account():
+    mails = [get_random_mail() for el in random.randrange(3,6)]
+    return Account(gen_text(3, '%sacc'), mails)
+
+def gen_text(length=8, formatter='', chars=string.ascii_letters + string.digits):
+    return ''.join([random.choice(chars) for i in range(length)])
 
 if __name__ == '__main__':
     pass
