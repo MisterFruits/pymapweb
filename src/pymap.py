a4827b8a750db77a57fe6fd58fa16df807a63afa
Victor <vic.toad.tor@free.fr>
2014-11-14 01:12:25 +0100

    CLEAN object definition

--- src/pymap.py
+++ src/pymap.py
@@ -3,16 +3,17 @@ Model for IMAP in python
 """
 #import email
 
-class Account:
+class Account(object):
     """Mail account"""
     def __init__(self, name, mails=None):
+        super(Account, self).__init__()
         self.name = name
         self.mails = mails or []
 
     def __str__(self):
         return 'Account %s: %d mails' % (self.name, len(self.mails))
 
-class Mail:
+class Mail(object):
     """Mail"""
     def __init__(self, header, body, attachements=None):
         super(Mail, self).__init__()
@@ -23,7 +24,7 @@ class Mail:
     def __str__(self):
         return self.header.__str__()
 
-class Header:
+class Header(object):
     """Mail header"""
     def __init__(self, subject, sender, recievers):
         super(Header, self).__init__()
